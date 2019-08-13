3934f29b9ea5cca254c70bcd54dcf374ee0be50a
eastandrew <38232641+eastandrew@users.noreply.github.com>
2019-08-13 14:42:14 -0400

    create to show rmd file

---
+++ app.R
@@ -0,0 +1,24 @@
+
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+library(knitr)
+
+rmdfiles <- c("SSDapp_tabset.Rmd")
+sapply(rmdfiles, knit, quiet = T)
+
+ui <- shinyUI(
+  fluidPage(
+    includeMarkdown("SDapp_tabset.Rmd")
+  )
+)
+server <- function(input, output) { }
+
+shinyApp(ui, server)
