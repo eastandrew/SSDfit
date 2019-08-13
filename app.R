08fe31c672ff3b97076fb6f45627642904741b6a
eastandrew <38232641+eastandrew@users.noreply.github.com>
2019-08-13 15:11:14 -0400

    Update app.R

--- app.R
+++ app.R
@@ -1,2 +1,14 @@
 
-rmarkdown::run("SSDapp_tabset.Rmd")
+#rmarkdown::run("SSDapp_tabset.Rmd")
+
+library(shiny)
+
+file <- 'SSDapp_tabset.Rmd'
+dir <- dirname(file)
+
+ui <- rmarkdown:::rmarkdown_shiny_ui(dir, file)
+render_args <- list()
+render_args$envir <- parent.frame()
+server <- rmarkdown:::rmarkdown_shiny_server(dir, file, 'UTF-8', T, render_args)
+
+shinyApp(ui, server)
