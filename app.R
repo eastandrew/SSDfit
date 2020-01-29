e163a6356e89c3c3f899a938e53a5c79a497148c
eastandrew <38232641+eastandrew@users.noreply.github.com>
2020-01-29 09:34:08 -0500

    remove UTF-8 as now default

--- app.R
+++ app.R
@@ -9,6 +9,6 @@ dir <- dirname(file)
 ui <- rmarkdown:::rmarkdown_shiny_ui(dir, file)
 render_args <- list()
 render_args$envir <- parent.frame()
-server <- rmarkdown:::rmarkdown_shiny_server(dir, file, 'UTF-8', T, render_args)
+server <- rmarkdown:::rmarkdown_shiny_server(dir, file, T, render_args)
 
 shinyApp(ui, server)
